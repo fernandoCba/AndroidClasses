1433f6c93e8f88acebdfb62db6cf1a8405483448
Fernando Rodriguez <ferchurodriguez@gmail.com>
2016-09-15 16:54:31 -0300

    Initial layout using a relative one.
    Adding spanish strings. and changing icons.

--- app/src/main/java/ar/edu/unc/famaf/activitiesassignment/NewsActivity.java
+++ app/src/main/java/ar/edu/unc/famaf/activitiesassignment/NewsActivity.java
@@ -1,18 +1,16 @@
-package famaf.unc.edu.ar.activitiesassignment;
+package ar.edu.unc.famaf.activitiesassignment;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
+
+import famaf.unc.edu.ar.activitiesassignment.R;
 
 public class NewsActivity extends AppCompatActivity {
     static final int LOGIN_REQUEST = 1000;
